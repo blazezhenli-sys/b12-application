0f403693bb1da01d88fcf59c6a159998e3064ec3
Blaze Zhenli <blazezhenli@gmail.com>
2026-01-22 19:38:15 +0800

    Added files

---
+++ main.py
@@ -0,0 +1,22 @@
+import requests
+
+def main_thread():
+    content = {
+  "timestamp": "2026-01-06T16:59:37.571Z",
+
+  "name": "Blaze Zhenli",
+
+  "email": "blazezhenli@gmail.com",
+
+  "resume_link": "https://drive.google.com/file/d/1bnbFZuMkc0bLBcdR6X7OyptLrv5IxLRQ/view?usp=drive_link",
+
+  "repository_link": "https://github.com/blazezhenli-sys/b12-application",
+
+  "action_run_link": "https://link-to-github-or-another-forge.example.com/your/repository/actions/runs/run_id"
+
+}
+    requests.post()
+    pass
+
+if __name__ == "__main__":
+    main_thread()
